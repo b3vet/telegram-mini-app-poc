64439345aaf2221d4261f37c75e16903be44cd57
b3vet <berkeucvet@gmail.com>
2024-07-05 01:02:43 +0300

    dummy app is ready

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Hello There Telegram! 🚀
+        </h1>
         <p>
           {window.Telegram.WebApp.initData}
         </p>
